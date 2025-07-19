caecf68c43b703a47955eea3e8150b84bc422d38
D4sik97 <danielpogosyan759@gmail.com>
2025-07-20 00:30:24 +0400

    uploadet files

--- controllers/view.js
+++ controllers/view.js
@@ -2,26 +2,31 @@ const Cat = require('../models/cat')
 const Product = require('./../models/product')
 
 module.exports = {
-  getHome: async (req,res) => {
-          const cat = await Cat.find()
-          const products = await Product.find();
-          res.render('index',{
-             title: 'Home',
-              categories: cat,
-              products
-          })
+    getHome: async (req, res) => {
+        const cat = await Cat.find()
+        const products = await Product.find();
+        res.render('index', {
+            title: 'Home',
+            categories: cat,
+            products
+        })
     },
-    getProduct: async (req,res) => {
-          const cat = await Cat.find()
-          res.render('product',{
-             title: 'Home',
-              categories: cat
-          })
+    getProduct: async (req, res) => {
+        const cat = await Cat.find()
+        res.render('product', {
+            title: 'Home',
+            categories: cat
+        })
     },
     getProductDetails: async (req, res) => {
-        res.render('product',{
+        const product = await Product.findById(req.params.id)
+        if (!product) return res.status(404).json({
+            message: 'Product not found'
+        });
+        res.render('product', {
             title: 'Home',
-            id: req.params.id
+            id: req.params.id,
+            product
         })
     }
 }
